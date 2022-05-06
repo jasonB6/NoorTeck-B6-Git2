e79d975965566fcaaebbd3421ffee12d5d222503
jason <jason@10.0.0.42>
2022-05-05 20:22:42 -0400

    +add method

--- src/test/java/git2/DemoTwo.java
+++ src/test/java/git2/DemoTwo.java
@@ -4,7 +4,11 @@ public class DemoTwo {
 	public static void main(String[] args) {
 
 		System.out.println("Git demo 2...");
-		
+
 		System.out.println("0 1 1 2 3 5 8 13 21 34 55 89");
+	}
+
+	public void add(int a, int b) {
+		System.out.println(a + b);
 	}
 }
