8c246c084635202931a86d7bb6169afc88eb0042
jasonB6 <96351555+jasonB6@users.noreply.github.com>
2022-05-05 20:42:24 -0400

    Update DemoTwo.java
    add comment for fibo print satement

--- src/test/java/git2/DemoTwo.java
+++ src/test/java/git2/DemoTwo.java
@@ -4,7 +4,7 @@ public class DemoTwo {
 	public static void main(String[] args) {
 
 		System.out.println("Git demo 2...");
-
+// Fibonacci sequence
 		System.out.println("0 1 1 2 3 5 8 13 21 34 55 89");
 		
 	}
